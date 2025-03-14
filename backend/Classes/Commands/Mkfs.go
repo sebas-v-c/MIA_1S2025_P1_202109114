113fa4f3935d9700e151c8a9598a5428faa0634e
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2025-03-14 10:14:29 -0600

    get mounted partition given id

--- backend/Classes/Commands/Mkfs.go
+++ backend/Classes/Commands/Mkfs.go
@@ -1,9 +1,12 @@
 package Commands
 
 import (
+	env "backend/Classes/Env"
 	"backend/Classes/Interfaces"
+	"backend/Classes/Structs"
 	"backend/Classes/Utils"
 	"errors"
+	"fmt"
 )
 
 type Mkfs struct {
@@ -27,6 +30,22 @@ func (m *Mkfs) Exec() {
 		m.AppendError(err.Error())
 		return
 	}
+
+	var consoleString string
+	consoleString = "=================MKFS=================\n"
+	consoleString += fmt.Sprintf("Id: %s, Type: %s, Fs: EXT2\n", m.Params["id"], m.Params["type"])
+
+	var mountedPartition *Structs.MountedPartition
+	if mountedPartition = m.getPartition(); mountedPartition == nil {
+		m.AppendError("Partition ID not found")
+		return
+	}
+
+	if mountedPartition.Status != [1]byte{'1'} {
+		m.AppendError("Partition is not mounted")
+		return
+	}
+
 }
 
 func (m *Mkfs) validateParams() error {
@@ -35,6 +54,15 @@ func (m *Mkfs) validateParams() error {
 	}
 	if _, ok := m.Params["type"]; !ok {
 		m.Params["type"] = "full"
+	}
+	return nil
+}
+
+func (m *Mkfs) getPartition() *Structs.MountedPartition {
+	for _, part := range env.GetPartitions() {
+		if string(part.Id[:]) == m.Params["id"] {
+			return part
+		}
 	}
 	return nil
 }
