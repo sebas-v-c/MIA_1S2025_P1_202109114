5f08ff70f1de8c6ae345d88ed0c2e88fd9a903d0
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2025-04-03 02:28:32 -0600

    godoc

--- backend/Classes/Structs/PointerBlock.go
+++ backend/Classes/Structs/PointerBlock.go
@@ -1,11 +1,16 @@
+// Package Structs provides data structures for use within the application.
 package Structs
 
 import "fmt"
 
+// PointerBlock represents a block of pointers in the filesystem.
+// It contains an array of 16 pointers represented as int32 values.
 type PointerBlock struct {
-	Pointers [16]int32
+	Pointers [16]int32 // Pointers holds an array of pointer values.
 }
 
+// ToString returns a string representation of the PointerBlock.
+// It formats the Pointers array into a readable string.
 func (p *PointerBlock) ToString() string {
 	return fmt.Sprintf("Pointers: %v", p.Pointers)
 }
